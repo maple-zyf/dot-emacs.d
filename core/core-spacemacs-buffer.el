6d7b6e401060a94fe640e9d5ea45281746c86a68
zeyife <zengyingfeng123@gmail.com>
2016-09-02 17:46:11 +0800

    save for read README.org

--- core/core-spacemacs-buffer.el
+++ core/core-spacemacs-buffer.el
@@ -14,7 +14,7 @@
 (defconst spacemacs-buffer-name "*spacemacs*"
   "The name of the spacemacs buffer.")
 
-(defconst spacemacs-buffer-logo-title "[S P A C E M A C S]"
+(defconst spacemacs-buffer-logo-title "[G N U | E M A C S]"
   "The title displayed beneath the logo.")
 
 (defconst spacemacs-buffer--banner-length 75
